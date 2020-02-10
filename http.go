91b665777949d155e36f319ce05eb10811a45e71
Teo Cheng Lim <chenglim.teo@thunes.com>
2020-02-10 13:54:25 +0800

    first commit

---
+++ http.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+var port int
+
+func init()  {
+	var port int
+	flag.IntVar(&port, "port", 8000, "specify port to use.  defaults to 8000.")
+	flag.Parse()
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./")))
+	http.HandleFunc("/ping", ping)
+	addr := fmt.Sprintf(":%d", port)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
+}
